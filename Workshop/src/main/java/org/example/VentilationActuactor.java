5361237f65cf99e63a040cabf74286f59cb53db6
SaraHende <Sarayh@hotmail.dk>
2023-11-27 11:58:04 +0100

    Created construtor in VentilationActuactor

--- Workshop/src/main/java/org/example/VentilationActuactor.java
+++ Workshop/src/main/java/org/example/VentilationActuactor.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.UUID;
+
 public class VentilationActuactor extends Actuator
 {
+    public VentilationActuactor(UUID id)
+    {
+        super(id);
+    }
+
     @Override
     public void setValue(double value) {
         super.setValue(value);
