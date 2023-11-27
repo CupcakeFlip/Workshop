018390bdef44f6df89aa9cf37b644fad7dcdf892
ThomasLeanderNielsen <thomasleandernielsen@gmail.com>
2023-11-27 10:42:24 +0100

    classes

---
+++ Workshop/src/main/java/org/example/Buildings.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Buildings {
+String Name;
+
+    
+
+}
--- Workshop/src/main/java/org/example/Main.java
+++ Workshop/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
+
+    Buildings build[];
+
+ 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 }
