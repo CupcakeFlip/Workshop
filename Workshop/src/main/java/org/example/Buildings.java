2c16f6a15e4dcf308ac81565c56bde791497e5ab
ThomasLeanderNielsen <thomasleandernielsen@gmail.com>
2023-11-27 11:05:01 +0100

    Main og buildings

--- Workshop/src/main/java/org/example/Buildings.java
+++ Workshop/src/main/java/org/example/Buildings.java
@@ -1,9 +1,19 @@
 package org.example;
+import java.util.*;
 
+import java.util.ArrayList;
 
 public class Buildings {
 String Name;
+int Id;
+List<Sensors>Sensors;
+List<Actuator>actuators;
 
-    
+public Buildings(String name , int id){
+Name=name;
+Id=id;
+Sensors = new ArrayList<Sensors>();
+actuators= new ArrayList<Actuator>();
+}
 
 }
